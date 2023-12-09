38d407e2f22f0875330ce1a02ef56b89ccd51013
ajrobbins04 <ajrobbins04@gmail.com>
2023-12-09 09:04:00 -0800

    character pool successfully filled

--- src/records.rs
+++ src/records.rs
@@ -8,4 +8,20 @@ pub mod records {
         pub username: String,
         pub password: String
     }
+
+    pub fn generate_password() -> Vec<char> {
+        // Define a vector containing characters for password generation
+        let chars_pool: Vec<char> = (b'A'..=b'Z')
+            // ..= makes the last range value inclusive
+            .chain(b'a'..=b'z') 
+            .chain(b'0'..=b'9')
+            .chain(b'!'..=b'/')
+            .chain(b':'..=b'@')
+            .chain(b'['..=b'`')
+            .chain(b'{'..=b'~')
+            .map(|c| c as char)
+            .collect();
+
+        chars_pool
+    } 
 }
