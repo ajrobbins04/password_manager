da8d15fefd8156832492e68a9e2542072a65ca95
ajrobbins04 <ajrobbins04@gmail.com>
2023-12-08 18:47:56 -0800

    first match loop coming along nicely

--- src/menu.rs
+++ src/menu.rs
@@ -16,32 +16,48 @@ pub mod menu {
             println!("4. Edit an Entry");
             println!("5. Delete an Entry");
             println!("6. Exit Password Manager");
+            println!(); // space between menu and input 
+            println!("Enter your selection from 1-6:");
 
+            // will need to allocate data from the heap for a String
             let input = get_input();
 
-            // convert to &str so a read-only view can be used
+            // convert to &str b/c string literals 
+            // like "1" are &str data types
             match input.as_str() {
                 "1" => {
-
+                    println!("1!");
+                    no_input = false;
                 }
                 "2" => {
-
+                    println!("2!");
+                    no_input = false;
                 }
                 "3" => {
-
+                    println!("3!");
+                    no_input = false;
                 }
                 "4" => {
-                    
+                    println!("4!");
+                    no_input = false;
                 }
                 "5" => {
-
+                    println!("5!");
+                    no_input = false;
                 }
                 "6" => {
-                    
+                    println!("6!");
+                    no_input = false;
                 }
                 _ => {
                     // wildcard alternative
+                    println!("anything!");
+                    no_input = false;
                 }
+            }
+            // is it time to break out of the loop?
+            if no_input == false {
+                break;
             }
         }
     }
@@ -53,8 +69,9 @@ pub mod menu {
         // passes in input as a mutable reference
         io::stdin().read_line(&mut input).expect("Failed to read line"); // .expect used for input stream error handling
 
-        // trim removes trailing whitespace
-        input.trim().to_string() // no final semicolon on lines that return data
+        // trim converts input to &str when removing output, 
+        // so to_string() must convert it back to a String
+        input.trim().to_string() // exclue a semicolon to return data
     }
   
 }
